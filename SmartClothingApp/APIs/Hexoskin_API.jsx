7ca421733065bf8508d34069993e7d472ba60c06
akul43 <46544707+akul43@users.noreply.github.com>
2023-06-12 16:43:48 -0700

    initial Hexoskin API exploration

---
+++ SmartClothingApp/APIs/Hexoskin_API.jsx
@@ -0,0 +1,33 @@
+// import axios from 'axios';
+
+// // Hexoskin API endpoint URL
+// const API_URL = 'https://api.hexoskin.com/api/v1/data';
+
+// // API key obtained from Hexoskin
+// const API_KEY = 'your-api-key';
+
+// // Function to fetch Hexoskin data
+// async function fetchHexoskinData() {
+//   try {
+//     const response = await axios.get(API_URL, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${API_KEY}`,
+//       },
+//       // Additional request parameters if required
+//       params: {
+//         // Example: date range for data retrieval
+//         start_date: '2022-01-01',
+//         end_date: '2022-01-31',
+//       },
+//     });
+//     // Handle the response data
+//     console.log(response.data);
+//   } catch (error) {
+//     // Handle errors
+//     console.error('Error fetching Hexoskin data:', error);
+//   }
+// }
+
+// // Call the function to fetch Hexoskin data
+// fetchHexoskinData();
