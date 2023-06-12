bc1ada385b35a59ce6adf076396bcb861682be39
akul43 <46544707+akul43@users.noreply.github.com>
2023-06-12 14:22:06 -0700

    Initial screen w/ functioning code

--- SmartClothingApp/App.js
+++ SmartClothingApp/App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+
+// Background Image placeholder
+const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      {/* Text content */}
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+
+      {/* Image container */}
+      <View style={styles.imageContainer}>
+        {/* Background Image */}
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+    </SafeAreaView>
   );
 }
 
+{/* Background Image */}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
