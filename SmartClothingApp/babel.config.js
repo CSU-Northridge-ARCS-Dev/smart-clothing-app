ac1d47135bda0713cc7ed7c4e75e94412a74fb8e
Akul <46544707+akul43@users.noreply.github.com>
2023-07-03 14:49:39 -0700

    Update babel.config.js

--- SmartClothingApp/babel.config.js
+++ SmartClothingApp/babel.config.js
@@ -1,16 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }]
+    ]
   };
-  plugins: [
-    ["module:react-native-dotenv", {
-      "moduleName": "@env",
-      "path": ".env",
-      "blacklist": null,
-      "whitelist": null,
-      "safe": false,
-      "allowUndefined": true
-  }]
- ]
 };
