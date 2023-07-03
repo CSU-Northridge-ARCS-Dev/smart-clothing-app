ded9375c4911a4f265659ed07ab75ee58690ee96
akul43 <46544707+akul43@users.noreply.github.com>
2023-07-03 12:41:08 -0700

    .env implementation

--- SmartClothingApp/babel.config.js
+++ SmartClothingApp/babel.config.js
@@ -3,4 +3,14 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
   };
+  plugins: [
+    ["module:react-native-dotenv", {
+      "moduleName": "@env",
+      "path": ".env",
+      "blacklist": null,
+      "whitelist": null,
+      "safe": false,
+      "allowUndefined": true
+  }]
+ ]
 };
