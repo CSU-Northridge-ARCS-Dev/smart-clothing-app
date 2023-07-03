9455040b4b3e8d7fb48ad8fd83d361535da690a2
Guru Vishnu <brguruvishnu22@gmail.com>
2023-07-03 22:31:15 +0530

    added image background

--- SmartClothingApp/assets/index.js
+++ SmartClothingApp/assets/index.js
@@ -1,3 +1,13 @@
 export const AppImages = {
   googleIcon: require("./images/google_icon.png"),
+  appLogo: require("./images/arcs_logo.png"),
+  bgImage1: require("./images/bg_image_1.jpg"),
+};
+export const AppFontsResource = {
+  ChakraPetch_Bold: require("./fonts/ChakraPetch-Bold.ttf"),
+  ChakraPetch_BoldItalic: require("./fonts/ChakraPetch-BoldItalic.ttf"),
+  ChakraPetch_Regular: require("./fonts/ChakraPetch-Regular.ttf"),
+  Poppins_Regular: require("./fonts/Poppins-Regular.ttf"),
+  Poppins_Bold: require("./fonts/Poppins-Bold.ttf"),
+  Poppins_Italic: require("./fonts/Poppins-Italic.ttf"),
 };
