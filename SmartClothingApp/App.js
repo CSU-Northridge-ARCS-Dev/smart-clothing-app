1db82c2d21f8d738a8b054974618ac8c2650f029
Guru Vishnu <brguruvishnu22@gmail.com>
2023-07-03 22:34:01 +0530

    expo font added

--- SmartClothingApp/App.js
+++ SmartClothingApp/App.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import AppRouter from "./src/navigation";
+import { useAppFonts } from "./src/hooks/useAppFonts";
+import { AppTheme } from "./src/constants/themes";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    const loadFont = async () => {
+      const res = await useAppFonts();
+      setLoading(false);
+    };
+    loadFont();
+  }, []);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <PaperProvider>
-        <NavigationContainer>
-          <AppRouter />
-        </NavigationContainer>
-      </PaperProvider>
-    </SafeAreaView>
+    <>
+      {!isLoading && (
+        <SafeAreaView style={{ flex: 1 }}>
+          <PaperProvider theme={AppTheme}>
+            <NavigationContainer>
+              <AppRouter />
+            </NavigationContainer>
+          </PaperProvider>
+        </SafeAreaView>
+      )}
+    </>
   );
 }
