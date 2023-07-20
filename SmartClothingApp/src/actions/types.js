e524eb8e8909ff2b9c9751e3d28d15eab4fcffee
Jeel <45571080+j33l@users.noreply.github.com>
2023-07-19 21:40:02 -0700

    Handling SignUp Errors from Firebase, Fixed Signup form Validation Errors display, Now using Better Functional Actions using Redux-Thunk

--- SmartClothingApp/src/actions/types.js
+++ SmartClothingApp/src/actions/types.js
@@ -1,3 +1,4 @@
 export const LOGIN_WITH_EMAIL = "LOGIN_WITH_EMAIL";
 export const SIGNUP_WITH_EMAIL = "SIGNUP_WITH_EMAIL";
 export const LOGOUT = "LOGOUT";
+export const AUTH_ERROR = "AUTH_ERROR";
--- SmartClothingApp/src/actions/userActions.js
+++ SmartClothingApp/src/actions/userActions.js
@@ -1,13 +1,22 @@
-import { LOGIN_WITH_EMAIL, SIGNUP_WITH_EMAIL, LOGOUT } from "./types";
+import { auth } from "../../firebaseConfig.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-export const loginWithEmail = (user) => {
+import {
+  LOGIN_WITH_EMAIL,
+  SIGNUP_WITH_EMAIL,
+  LOGOUT,
+  AUTH_ERROR,
+} from "./types";
+
+const loginWithEmail = (user) => {
   return {
     type: LOGIN_WITH_EMAIL,
     payload: user,
   };
 };
 
-export const signupWithEmail = (user) => {
+const signupWithEmail = (user) => {
+  console.log(" ##### Calling the Actual Action function ##### ");
   return {
     type: SIGNUP_WITH_EMAIL,
     payload: user,
@@ -17,5 +26,49 @@ export const signupWithEmail = (user) => {
 export const logout = () => {
   return {
     type: LOGOUT,
+  };
+};
+
+export const setAuthError = (errorMessage) => {
+  return {
+    type: AUTH_ERROR,
+    payload: errorMessage,
+  };
+};
+
+// useEffect(() => {
+//   const unsubscribe = auth.onAuthStateChanged((user) => {
+//     if (user) {
+//       // navigation.navigate("HomeScreen");
+//     }
+//   });
+
+//   return unsubscribe;
+// }, []);
+
+export const startSignupWithEmail = (email, password) => {
+  console.log(" ##### Calling the Start-Action function ##### ");
+  return (dispatch) => {
+    console.log(" ##### Calling the CreateUSer Firebase function ##### ");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        // console.log("User created successfully!");
+        // console.log(user);
+        dispatch(
+          signupWithEmail({
+            uuid: user.uid,
+            firstName: user.displayName?.split(" ")[0],
+            lastName: user.displayName?.split(" ")[1],
+            email: user.email,
+          })
+        );
+      })
+      .catch((error) => {
+        //TODO: Setup Good Error Message (https://firebase.google.com/docs/auth/admin/errors)
+        // console.log("Error creating user!");
+        // console.log(error);
+        dispatch(setAuthError(error.message));
+      });
   };
 };
