23316d920a2d0fb05badd77e48a808663bcaa540
Guru Vishnu <brguruvishnu22@gmail.com>
2023-07-26 22:59:45 +0530

    export components

--- SmartClothingApp/src/components/index.js
+++ SmartClothingApp/src/components/index.js
@@ -1,5 +1,6 @@
+import DeviceCard from "./AppCards/DeviceCard";
 import AppHeader from "./AppHeader";
 import GoogleButton from "./GoogleButton";
 import HeroSection from "./HeroSection";
 
-export { GoogleButton, HeroSection, AppHeader };
+export { GoogleButton, HeroSection, AppHeader, DeviceCard };
