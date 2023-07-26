7d81f76c05bca9ef490d81ec9e1dbb0acf8de8d7
Guru Vishnu <brguruvishnu22@gmail.com>
2023-07-26 23:01:57 +0530

    UI changes

--- SmartClothingApp/src/screens/HomeScreen/index.jsx
+++ SmartClothingApp/src/screens/HomeScreen/index.jsx
@@ -1,24 +1,55 @@
 import React from "react";
-import { Button, View, Text, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, ScrollView, StyleSheet } from "react-native";
 
 import { auth } from "../../../firebaseConfig.js";
 import { AppHeader } from "../../components";
+import { Button, Text } from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialIcons.js";
+import { AppColor, AppFonts, AppStyle } from "../../constants/themes.js";
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View>
+    <ScrollView style={styles.container}>
       <AppHeader title={"Dashboard"} />
-      <Text>Home Screen</Text>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity
-        onPress={() => {
-          auth.signOut().then(() => {
-            navigation.navigate("SignIn"); //TODO: Not working because of the AuthStack implementation which is incomplete and needs Redux setup
-          });
-        }}
-      >
-        <Text>Log Out</Text>
-      </TouchableOpacity>
-    </View>
+      <View style={styles.body}>
+        <Text style={AppStyle.title}>Hello, User</Text>
+        <View style={styles.insights}>
+          <Text
+            style={[AppStyle.subTitle, { fontFamily: AppFonts.chakraBold }]}
+          >
+            Weekly Summary
+          </Text>
+          <Button
+            mode="contained"
+            onPress={() => navigation.navigate("Insights")}
+            icon={"arrow-right"}
+            uppercase
+            contentStyle={{ flexDirection: "row-reverse" }}
+            style={{ alignSelf: "flex-end" }}
+          >
+            View
+          </Button>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  body: {
+    padding: 10,
+  },
+  insights: {
+    marginVertical: 10,
+    elevation: 2,
+    shadowColor: AppColor.shadow,
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    shadowOffset: { width: 5, height: 5 },
+    backgroundColor: AppColor.primaryContainer,
+    padding: 10,
+  },
+});
