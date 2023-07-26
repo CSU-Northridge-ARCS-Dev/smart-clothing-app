3b76b469587a3c23a0121420a68d5a09c3085f65
Guru Vishnu <brguruvishnu22@gmail.com>
2023-07-26 23:01:37 +0530

    initialise mydevice screen

--- SmartClothingApp/src/screens/MyDevices/index.jsx
+++ SmartClothingApp/src/screens/MyDevices/index.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
-import { AppHeader } from "../../components";
+import { Button, View, Text, StyleSheet, ScrollView } from "react-native";
+import { AppHeader, DeviceCard } from "../../components";
+import { devices } from "../../constants/data";
 
-export default function MyDevices({ navigation }) {
+const MyDevices = ({ navigation }) => {
   return (
-    <View style={{ flex: 1 }}>
-      <AppHeader title={"My Devices"} back={true} />
-      <Text>My Devices</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <AppHeader title={"My Devices"} />
+      <View style={styles.body}>
+        {devices.map((item, index) => (
+          <DeviceCard
+            key={index}
+            style={{ margin: 5 }}
+            device={item}
+            onPress={(e) => console.log(e)}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
-}
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  body: {
+    padding: 10,
+  },
+});
+export default MyDevices;
