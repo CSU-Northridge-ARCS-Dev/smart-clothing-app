62a687f5f63ea024924570b714977c811315fe1a
Guru Vishnu <brguruvishnu22@gmail.com>
2023-08-01 23:22:12 +0530

    added  toase message reducer and component

--- SmartClothingApp/src/actions/types.js
+++ SmartClothingApp/src/actions/types.js
@@ -3,3 +3,17 @@ export const SIGNUP_WITH_EMAIL = "SIGNUP_WITH_EMAIL";
 export const LOGOUT = "LOGOUT";
 export const AUTH_ERROR = "AUTH_ERROR";
 export const UPDATE_PROFILE = "UPDATE_PROFILE";
+
+export const AuthActionTypes = {
+  LOGIN_WITH_EMAIL: "LOGIN_WITH_EMAIL",
+  SIGNUP_WITH_EMAIL: "SIGNUP_WITH_EMAIL",
+  LOGOUT: "LOGOUT",
+  AUTH_ERROR: "AUTH_ERROR",
+  UPDATE_PROFILE: "UPDATE_PROFILE",
+};
+
+export const ToastTypes = {
+  SHOW_TOAST: "showToast",
+  SHOW_ERROR_TOAST: "showErrorToast",
+  DISCARD_TOAST: "discardToast",
+};
