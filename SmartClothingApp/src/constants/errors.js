c639d743149a2d3c6de56e843a645bbb5967d4af
cfiguer055 <cfiguer055@gmail.com>
2023-09-19 22:31:27 -0700

    package.json mod extensions and SigninScreen test

---
+++ SmartClothingApp/src/constants/errors.js
@@ -0,0 +1,14 @@
+export const ErrorMessages = {
+  messages: {
+    doesNotExist: "", //for Matthew
+    emailExists: "", //for Matthew
+    internalError: "Firebase: Error (auth/internal-error).",
+    wrongPass: "Firebase: Error (auth/wrong-password).",
+    invalidEmail: "Enter valid email!",
+    emptyPass: "Password cannot be empty!",
+    passReq: "Password length cannot be less than 6!",
+    emptyFirst: "Firstname cannot be empty!",
+    emptyLast: "Lastname cannot be empty!",
+    passMatch: "Passwords did not match!",
+  },
+};
