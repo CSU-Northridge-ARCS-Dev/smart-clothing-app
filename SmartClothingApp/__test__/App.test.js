7ae3a71ede18d22a327e3896dc91f257e2e7fd79
cfiguer055 <cfiguer055@gmail.com>
2023-09-24 12:56:24 -0700

    SigninScreen HelperText Test

--- SmartClothingApp/__test__/App.test.js
+++ SmartClothingApp/__test__/App.test.js
@@ -1,10 +1,10 @@
-// import React from 'react';
-// import renderer from 'react-test-renderer';
-// import App from '../App';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
 
-// describe('App component <App />', () => {
-//     it('renders correctly', () => {
-//         const tree = renderer.create(<App />).toJSON(); 
-//         expect(tree).toMatchSnapshot();
-//     });
-// });
+describe('App component <App />', () => {
+    it('renders correctly', () => {
+        const tree = renderer.create(<App />).toJSON(); 
+        expect(tree).toMatchSnapshot();
+    });
+});
--- SmartClothingApp/babel.config.js
+++ SmartClothingApp/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo'
+    ],
+    sourceType: 'unambiguous',
     plugins: [
       ["module:react-native-dotenv", {
         "moduleName": "@env",
