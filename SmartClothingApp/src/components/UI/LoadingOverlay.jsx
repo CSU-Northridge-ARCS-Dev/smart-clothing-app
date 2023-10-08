81e4333bc9114b82334f875668977134ca665cda
imaqtdinh <thomas98dinh@gmail.com>
2023-10-08 09:23:46 -0700

    add clear button, tweaks

--- SmartClothingApp/src/components/UI/LoadingOverlay.jsx
+++ SmartClothingApp/src/components/UI/LoadingOverlay.jsx
@@ -1,9 +1,10 @@
 import { View, ActivityIndicator, StyleSheet } from "react-native";
+import { AppColor } from "../../constants/themes";
 
 function LoadingOverlay() {
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="white" />
+      <ActivityIndicator size="large" color="#1560a4" />
     </View>
   );
 }
@@ -12,10 +13,10 @@ export default LoadingOverlay;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: AppColor.background,
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     padding: 24,
-    backgroundColor: "blue",
   },
 });
