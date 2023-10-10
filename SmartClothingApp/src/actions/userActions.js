4d393a399e74a80589dce0a5daca981f0fc2528d
imaqtdinh <thomas98dinh@gmail.com>
2023-10-10 13:40:59 -0700

    Display sports

--- SmartClothingApp/src/actions/userActions.js
+++ SmartClothingApp/src/actions/userActions.js
@@ -139,8 +139,9 @@ export const startLoadUserData = () => {
         const defaultUserData = {
           height: "",
           weight: "",
-          dob: "",
+          age: "",
           gender: "",
+          sports: "",
         };
         dispatch(startUpdateUserData(defaultUserData, auth.currentUser.uid));
       }
