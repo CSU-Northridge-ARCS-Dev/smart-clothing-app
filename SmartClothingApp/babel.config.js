9908d5f7cca45b9d83c7dc73f549afbcd97b41d0
imaqtdinh <thomas98dinh@gmail.com>
2023-10-30 12:36:14 -0700

    handle persistence

--- SmartClothingApp/babel.config.js
+++ SmartClothingApp/babel.config.js
@@ -1,16 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "blacklist": null,
-        "whitelist": null,
-        "safe": false,
-        "allowUndefined": true
-      }]
-    ]
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+        "react-native-reanimated/plugin",
+      ],
+    ],
   };
 };
--- SmartClothingApp/src/screens/ViewInsights/index.jsx
+++ SmartClothingApp/src/screens/ViewInsights/index.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, StyleSheet, ScrollView } from "react-native";
 import { AppHeader } from "../../components";
+import { AppStyle, AppFonts } from "../../constants/themes";
 
-export default function ViewInsights({ route }) {
+const ViewInsights = ({ route }) => {
   const { previousScreenTitle } = route.params;
   return (
-    <View style={{ flex: 1 }}>
+    <ScrollView style={{ flex: 1 }}>
       <AppHeader title={previousScreenTitle} back={true} />
-      <Text>Insights</Text>
-    </View>
+      <View style={styles.content}></View>
+    </ScrollView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 36,
+    marginVertical: 24,
+    textAlign: "center",
+    paddingTop: 45,
+  },
+  content: {
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    marginBottom: 20,
+  },
+});
+
+export default ViewInsights;
