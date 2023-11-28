eccbafd56388f8670c9c2251b88910686d262cb6
Emi Anyakpor <eanyakpor@gmail.com>
2023-11-28 12:12:27 -0800

    in app.js rmv call to ios exclusive functions

--- SmartClothingApp/App.js
+++ SmartClothingApp/App.js
@@ -22,11 +22,9 @@ import SplashScreen from "react-native-splash-screen";
 import { checkHealthKitAvailability } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
 import { requestHealthKitAuthorization } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
 import { getHeartRateData } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
-import { getActivityRingsData } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
 import { getRestingHeartRateData } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
-import { getActiveEnergyData } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
-import { getSleepData } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
 import { getHeartRateVariabilityData } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
+import { getActiveEnergyData } from "./src/utils/AppleHealthKit/AppleHealthKitUtils";
 
 const store = configureStore();
 
@@ -118,55 +116,38 @@ export default function App() {
     // return () => unsubscribe();
 
    // check if AppleHealthKit is Avaliable for use in the app
-    checkHealthKitAvailability()
-      .catch(error => {
-        console.error(error);
-      });
-
-    requestHealthKitAuthorization()
-      .catch(error => {
-        console.error(error);
-      });
-
-      // getActivityRingsData()
-      // .catch(error => {
-      //   console.error(error);
-      // });
-      // getActiveEnergyData()
-      // .catch(error => {
-      //   console.error(error);
-      // });
-      // getSleepData()
-      // .catch(error => {
-      //   console.error(error);
-      // });
-      getHeartRateVariabilityData()
-      .catch(error => {
-        console.error(error);
-      });
-    
-      // getHeartRateData()
-      //   .catch(error => {
-      //     console.error(error);
-      //   });
-      // getVO2MaxData()
-      // .catch(error => {
-      //   console.error(error);
-      // });
-
-    // getRestingHeartRateData()
+    // checkHealthKitAvailability()
     //   .catch(error => {
     //     console.error(error);
     //   });
 
-    // getHeartRateData()
-    //   .then((data) => {
-    //     setHeartRateDataArr(data);
-    //   })
-    //   .catch((error) => {
+    // requestHealthKitAuthorization()
+    //   .catch(error => {
     //     console.error(error);
     //   });
-    
+    // getHeartRateVariabilityData()
+    // .catch(error => {
+    //   console.error(error);
+    // });
+  
+    // getHeartRateData()
+    //   .catch(error => {
+    //     console.error(error);
+    //   });
+
+  // getRestingHeartRateData()
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+  // getSleepData()
+  // .catch(error => {
+  //   console.error(error);
+  // });
+
+  // getActiveEnergyData()
+  // .catch(error => {
+  //   console.error(error);
+  // });
 
   }, []);
 
