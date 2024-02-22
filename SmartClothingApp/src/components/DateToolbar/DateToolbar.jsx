aa877e095055fa5b3f459cf02766d78ef6aafcda
Thomas Dinh <thomas98dinh@gmail.com>
2024-02-21 16:59:31 -0800

    Date range redux

--- SmartClothingApp/src/components/DateToolbar/DateToolbar.jsx
+++ SmartClothingApp/src/components/DateToolbar/DateToolbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback, useMemo, useRef } from "react";
 import {
   Button,
   View,
@@ -11,53 +11,80 @@ import { AppHeader } from "../../components";
 import ActivityRings from "../../components/visualizations/ActivityRings/ActivityRings";
 import { AppColor, AppStyle, AppFonts } from "../../constants/themes";
 import { useSelector, useDispatch } from "react-redux";
-import { updateActivityRings } from "../../actions/appActions";
 import DailyInsights from "../../components/DailyInsights/DailyInsights";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import ActivityChart from "../../components/visualizations/ActivityChart/ActivityChart";
 import { useFocusEffect } from "@react-navigation/native";
+import DateRangePicker from "./DateRangePicker";
+import { updateDateRange } from "../../actions/appActions";
 
 const DateToolbar = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
+  const dates = useSelector((state) => state.app.dateRangeData);
+  const dispatch = useDispatch();
+  const [dateRange, setDateRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+  })
+
+  const handleDateRangeSuccess = (startDate, endDate) => {
+    // Handle the selected date range here
+    const startDateString = new Date(startDate).toISOString();
+    const endDateObject = new Date(endDate)
+
+    endDateObject.setHours(39);
+    endDateObject.setMinutes(59);
+    endDateObject.setSeconds(59);
+    endDateObject.setMilliseconds(999);
+
+    const endDateString = endDateObject.toISOString();
+
+    setDateRange({
+      startDate: startDateString,
+      endDate: endDateString
+      });
+  };
 
   const onChangeDate = (event, selectedDate) => {
     setShowDatePicker(Platform.OS === "ios");
     if (selectedDate) {
-      setCurrentDate(selectedDate);
+      setSelected(selectedDate);
     }
   };
 
-  const formattedDate = currentDate.toLocaleDateString("en-US", {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
+  const handleUpdate = async () => {
+    await dispatch(updateDateRange(dateRange.startDate, dateRange.endDate));
+  }
+
+  // const formattedDate = selected.toLocaleDateString("en-US", {
+  //   weekday: "long",
+  //   month: "long",
+  //   day: "numeric",
+  //   year: "numeric",
+  // });
+
+  useEffect(() => {
+    handleUpdate();
+  }, [dateRange]);
 
   return (
     <>
       <View style={styles.dateContainer}>
-        <Text style={styles.title}>{formattedDate}</Text>
+        {/* <Text style={styles.title}>{formattedDate}</Text> */}
         <View style={styles.iconContainer}>
           <Icon
             name="calendar-alt"
             size={20}
             style={styles.icon}
-            onPress={() => setShowDatePicker(true)}
+            onPress={() => setShowDatePicker(!showDatePicker)}
           />
           <Icon name="sliders-h" size={20} style={styles.icon} />
           <Icon name="upload" size={20} style={styles.icon} />
         </View>
       </View>
       {showDatePicker && (
-        <DateTimePicker
-          value={currentDate}
-          mode="date"
-          display="default"
-          onChange={onChangeDate}
-        />
+        <DateRangePicker onSuccess={handleDateRangeSuccess}/>
       )}
     </>
   );
