970b6677dd7a38289b2b246ab58d7137c3111f71
Viper-one-one <taylor.j.nastally@gmail.com>
2024-03-30 01:46:05 -0700

    Update dependencies and fix registry error

--- SmartClothingApp/index.js
+++ SmartClothingApp/index.js
@@ -6,4 +6,5 @@ import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent("SmartClothingApp", () => App);
+AppRegistry.registerComponent(appName, () => App);
+// replaced "SmartClothingApp" with appName bc of registry error on the string
