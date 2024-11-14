0de3836409ba535ceedcefa225f8250859a41085
cfiguer055 <cfiguer055@gmail.com>
2024-11-13 19:23:38 -0800

    SignupToHome.test PASS

--- SmartClothingApp/__test__/__integration-test__/__auth-stack-test__/__auth-navigation-test__/SignupToHome.test.js
+++ SmartClothingApp/__test__/__integration-test__/__auth-stack-test__/__auth-navigation-test__/SignupToHome.test.js
@@ -60,7 +60,22 @@ jest.mock('../../../../firebaseConfig.js', () => ({
 
 jest.mock('../../../../src/utils/localStorage.js', () => ({
   AsyncStorage: jest.fn(),
+  storeUID: jest.fn(),
+  storeMetrics: jest.fn(),
+  getUID: jest.fn(),
+  clearUID: jest.fn(),
+  getMetrics: jest.fn(),
+  clearMetrics: jest.fn()
 }));
+
+// Mock AsyncStorage
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  getItem: jest.fn(() => Promise.resolve('mocked_value')),
+  setItem: jest.fn(() => Promise.resolve()),
+  removeItem: jest.fn(() => Promise.resolve()),
+  clear: jest.fn(() => Promise.resolve()),
+}));
+
 
 // jest.mock('../../src/actions/toastActions.js', () => ({
 //     toastError: jest.fn(() => Promise.resolve()),
@@ -111,7 +126,7 @@ jest.mock('../../../../src/actions/appActions.js', () => {
 jest.mock('firebase/firestore', () => ({
     collection: jest.fn(() => ({ add: jest.fn() })),
     addDoc: jest.fn(),
-    setDoc: jest.fn(),
+    setDoc: jest.fn(() => Promise.resolve()),
     doc: jest.fn(() => ({ setDoc: jest.fn() })),
     updateDoc: jest.fn(),
     getDoc: jest.fn().mockReturnValue({
